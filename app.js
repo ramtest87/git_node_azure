2aaee8252869a1475aec62c78cd3c0bb83b3afbb
ram291787 <ram291787@gmail.com>
2021-06-09 21:26:26 +0530

    sixth commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var app = express()
 
+var port = process.env.PORT || 3000;
+
 app.get('/',function(req,res){
 
     console.log("test")
     res.json({"Name":"Ram"})
 })
 
-app.listen(3000,function(){console.log("server started.....")})
+app.listen(port,function(){console.log("server started....."+port)})
