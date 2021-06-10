fee4f000f17592db26b08fd6b4bc4af7fc48156b
ram291787 <ram291787@gmail.com>
2021-06-10 12:30:56 +0530

    html file added

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var express = require('express')
+var bodyParser = require("body-parser");
 var app = express()
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 var port = process.env.PORT || 3000;
 
 app.get('/',function(req,res){
-
     console.log("test")
-    res.json({"Name":"Ram"})
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+
+app.get('/sample',function(req,res){
+    console.log("sample")
+    res.sendFile(__dirname + "/public/sample.html");
 })
 
 app.listen(port,function(){console.log("server started....."+port)})
